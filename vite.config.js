70a9af0947d0ab3cc6cee103e78df8b1643edaf9
Skyak <akashthakkar0701@hotmail.com>
2026-06-04 08:38:00 -0400

    Fix GitHub Pages asset base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  base: "/akash-thakkar-portfolio/",
   plugins: [react()],
 });
